2a6ce0b36f2d2b87716d010825a8b6242d5ae0f3
KJ Sudarshan <kjsudi@gmail.com>
2022-11-26 17:24:09 +0530

    update: wip menu icon for mobile devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import styles from "./Navbar.module.scss";
 import LogoPath from "src/assets/icons/logo.svg";
 import Link from "next/link";
+import { useMediaQuery } from "react-responsive";
 
 const Navbar = () => {
   const handleContactClick = () => {
     document?.querySelector("footer")?.scrollIntoView({ behavior: "smooth" });
   };
 
+  const [isMediumScreen, setIsMediumScreen] = useState<boolean | null>(null);
+  const isScreenWidthGreaterThan768px = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
+
+  /*it may look like we are unnecessarily using a state variable and useEffect here, but this is done to counter the rehydration
+   problem mentioned here: https://nextjs.org/docs/messages/react-hydration-error*/
+
+  useEffect(() => {
+    setIsMediumScreen(isScreenWidthGreaterThan768px);
+  }, [isScreenWidthGreaterThan768px]);
   return (
     <header className={styles.container}>
       <Link href="/">
@@ -16,22 +29,29 @@ const Navbar = () => {
       </Link>
       {/*TODO: Create hamburger for mobile devices*/}
       <nav className={styles.actions}>
-        <h4>
-          <Link href="/solutions/hardware-and-embedded-security">
-            solutions
-          </Link>
-        </h4>
-        <h4>
-          <Link target="_blank" href="https://medium.com/deep-armor">
-            blog
-          </Link>
-        </h4>
-        <h4>
-          <Link href="/about-us">about us</Link>
-        </h4>
-        <h4 className={styles.contact} onClick={handleContactClick}>
-          contact
-        </h4>
+        {isMediumScreen && (
+          <>
+            <h4>
+              <Link href="/solutions/hardware-and-embedded-security">
+                solutions
+              </Link>
+            </h4>
+            <h4>
+              <Link target="_blank" href="https://medium.com/deep-armor">
+                blog
+              </Link>
+            </h4>
+            <h4>
+              <Link href="/about-us">about us</Link>
+            </h4>
+            <h4 className={styles.contact} onClick={handleContactClick}>
+              contact
+            </h4>
+          </>
+        )}
+        {isMediumScreen === false && (
+          <div className={styles.menuIcon}>Hamburger Icon</div>
+        )}
       </nav>
     </header>
   );
