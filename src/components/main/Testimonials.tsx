6409dae2f90a7ebcc658b60d8cdb5cde5a3edeb5
KJ Sudarshan <kjsudi@gmail.com>
2022-12-08 10:00:54 +0530

    add: testimonials

--- src/components/main/Testimonials.tsx
+++ src/components/main/Testimonials.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import styles from "./Testimonials.module.scss";
 import TestimonialCard from "./TestimonialCard";
 import hardware from "src/assets/images/hardware.jpg";
+import sumanth from "src/assets/images/man-sumanth.png";
+import sridhar from "src/assets/images/man-sridhar.jpg";
+import mobile from "src/assets/images/mobile.jpg";
+import signature from "src/assets/images/signature.jpg";
+import training from "src/assets/images/training.jpg";
 import { TestimonialType } from "src/types";
 import { useState } from "react";
 import ArrowRight from "src/assets/icons/arrow-right.svg";
 import ArrowLeft from "src/assets/icons/arrow-left.svg";
+import { useMediaQuery } from "react-responsive";
 
 const testimonials: TestimonialType[] = [
   {
@@ -18,35 +25,35 @@ const testimonials: TestimonialType[] = [
     id: 1,
     from: "Second, McAfee",
     description:
-      "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
-    image: hardware,
+      "Eiusmod ex excepteur aliqua reprehenderit mollit cillum cillum magna.",
+    image: sumanth,
   },
   {
     id: 2,
     from: "Third, Cisco",
     description:
-      "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
-    image: hardware,
+      "Eu cillum consectetur voluptate Lorem fugiat enim in cillum nostrud minim. ",
+    image: sridhar,
   },
   {
     id: 3,
     from: "Fourth, Dark Labs",
     description:
-      "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
-    image: hardware,
+      "Magna consequat incididunt nisi do ipsum incididunt veniam do. Dolor incididunt labore non laboris. ",
+    image: mobile,
   },
   {
     id: 4,
     from: "Fifth, Alibaba",
     description:
       "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
-    image: hardware,
+    image: mobile,
   },
   {
     id: 5,
     from: "Sixth, McAfee",
     description:
-      "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
+      "Eu cillum consectetur voluptate Lorem fugiat enim in cillum nostrud minim. ",
     image: hardware,
   },
   {
@@ -54,14 +61,14 @@ const testimonials: TestimonialType[] = [
     from: "Seventh, Cisco",
     description:
       "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
-    image: hardware,
+    image: signature,
   },
   {
     id: 7,
     from: "Eigth, Dark Labs",
     description:
-      "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
-    image: hardware,
+      "Dolore eiusmod fugiat reprehenderit mollit excepteur qui ullamco non ut. ",
+    image: training,
   },
 
   {
@@ -83,18 +90,30 @@ const testimonials: TestimonialType[] = [
     from: "Eleventh, Dark Labs",
     description:
       "Occaecat pariatur occaecat nostrud culpa adipisicing sunt. Deserunt amet labore non aute Lorem.",
-    image: hardware,
+    image: training,
   },
 ];
 
-const MAX_TESTIMONIALS_PER_SECTION = 4;
-
 const Testimonials = () => {
   const [testimonialSection, setTestimonialSection] = useState(0);
+  const [isMediumScreen, setIsMediumScreen] = useState<boolean | null>(null);
+
+  let MAX_TESTIMONIALS_PER_SECTION = isMediumScreen ? 4 : 2;
+
   const slicedTestimonials = testimonials.slice(
     MAX_TESTIMONIALS_PER_SECTION * testimonialSection,
     MAX_TESTIMONIALS_PER_SECTION * (testimonialSection + 1)
   );
+
+  const isScreenWidthGreaterThan768px = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
+
+  /*it may look like we are unnecessarily using a state variable and useEffect here, but this is done to counter the rehydration
+   problem mentioned here: https://nextjs.org/docs/messages/react-hydration-error*/
+  useEffect(() => {
+    setIsMediumScreen(isScreenWidthGreaterThan768px);
+  }, [isScreenWidthGreaterThan768px]);
 
   const handleNextClick = () => {
     setTestimonialSection((p) => p + 1);
@@ -120,18 +139,19 @@ const Testimonials = () => {
         {slicedTestimonials.map((testimonial) => (
           <TestimonialCard {...testimonial} />
         ))}
+        <div className={styles.controls}>
+          <button
+            disabled={shouldDisabledPreviousClick}
+            onClick={handlePreviousClick}
+          >
+            <ArrowLeft />
+          </button>
+          <button disabled={shouldDisabledNextClick} onClick={handleNextClick}>
+            <ArrowRight />
+          </button>
+        </div>
       </div>
-      <div className={styles.controls}>
-        <button
-          disabled={shouldDisabledPreviousClick}
-          onClick={handlePreviousClick}
-        >
-          <ArrowLeft />
-        </button>
-        <button disabled={shouldDisabledNextClick} onClick={handleNextClick}>
-          <ArrowRight />
-        </button>
-      </div>
+
       <div className={styles.indicators}>
         {Array(totalNumberOfSections)
           .fill(0)
