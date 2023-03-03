a0367a1df42e478e06c10b508f2fc12b6044c42f
KJ Sudarshan <kjsudi@gmail.com>
2023-03-03 14:08:47 +0530

    expand hero content and nav bar width

--- src/components/group/main/Hero.tsx
+++ src/components/group/main/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <h2>
             Deep-tech <span>security.</span>
           </h2>
-          <h2>Peace of mind</h2>
+          <h2>Peace of mind.</h2>
           <p>
             Award-winning cybersecurity firm. Trusted partner to over 35
             companies across industry sectors & geographies.
